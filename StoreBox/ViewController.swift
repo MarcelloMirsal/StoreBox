470bf5acbedb0b7c14171d07b0ab7627f659d456
Marcello <mrmotherboard14@gmail.com>
2020-05-04 05:35:21 +0300

    Initial Commit

---
+++ StoreBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreBox
+//
+//  Created by Marcello Mirsal on 04/05/2020.
+//  Copyright © 2020 Mohammed Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoreBoxTests/StoreBoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  StoreBoxTests.swift
+//  StoreBoxTests
+//
+//  Created by Marcello Mirsal on 04/05/2020.
+//  Copyright © 2020 Mohammed Ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import StoreBox
+
+class StoreBoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
