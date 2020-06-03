88a6b47f80e27c254df13728b6cb0951d586606b
Marcello <mrmotherboard14@gmail.com>
2020-06-03 17:06:09 +0300

    basic UI design for MoreViewController

---
+++ StoreBox/MoreViewController/MoreViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MoreViewController.swift
+//  StoreBox
+//
+//  Created by Marcello Mirsal on 02/06/2020.
+//  Copyright © 2020 Mohammed Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UITableViewController {
+}
