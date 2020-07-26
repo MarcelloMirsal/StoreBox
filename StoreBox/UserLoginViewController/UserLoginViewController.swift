4f22da2104b00737e9614f58503120b2206026fb
Marcello <mrmotherboard14@gmail.com>
2020-07-26 16:57:39 +0300

    Basic Design For UserLoginViewController

---
+++ StoreBox/UserLoginViewController/UserLoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserLoginViewController.swift
+//  StoreBox
+//
+//  Created by Marcello Mirsal on 26/07/2020.
+//  Copyright © 2020 Mohammed Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class UserLoginViewController: UIViewController {
+    
+    
+    @IBAction func presentMainStoryboard() {
+        present(getMainTabBarController() , animated: true)
+    }
+    
+    func getMainTabBarController() -> UITabBarController {
+        let mainTabBarController = UIStoryboard(name: "Main").getInitialViewController(of: UITabBarController.self)
+        mainTabBarController.modalPresentationStyle = .fullScreen
+        return mainTabBarController
+    }
+    
+    
+}
---
+++ StoreBoxTests/UserLoginViewControllerTests/UserLoginViewControllerTests.swift
@@ -0,0 +1,29 @@
+//
+//  UserLoginViewControllerTests.swift
+//  StoreBoxTests
+//
+//  Created by Marcello Mirsal on 26/07/2020.
+//  Copyright © 2020 Mohammed Ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import StoreBox
+
+class UserLoginViewControllerTests: XCTestCase {
+    
+    var sut: UserLoginViewController!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = UIStoryboard(name: "UserLoginViewController").getInitialViewController(of: UserLoginViewController.self)
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testPresentMainStoryboard() { // UI Details
+        sut.presentMainStoryboard()
+    }
+    
+}
